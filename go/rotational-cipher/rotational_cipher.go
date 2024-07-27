c7e6dc36578e0cc4731af317c0e254bb1cb58d5b
Luís Felipe Bilecki <luis.bilecki@gmail.com>
2024-07-27 10:26:23 -0300

    feat: add rotational cipher example

--- go/rotational-cipher/rotational_cipher.go
+++ go/rotational-cipher/rotational_cipher.go
@@ -1,5 +1,23 @@
 package rotationalcipher
 
+import (
+	"unicode"
+)
+
+const AlphabetSize = 26
+const UpperCaseA = 65
+const LowerCaseA = 97
+
 func RotationalCipher(plain string, shiftKey int) string {
-	panic("Please implement the RotationalCipher function")
+	cipher := ""
+	for _, char := range plain {
+		if unicode.IsUpper(char) {
+			cipher += string((char+rune(shiftKey)-rune(UpperCaseA))%AlphabetSize + UpperCaseA)
+		} else if unicode.IsLower(char) {
+			cipher += string((char+rune(shiftKey)-rune(LowerCaseA))%AlphabetSize + LowerCaseA)
+		} else {
+			cipher += string(char)
+		}
+	}
+	return cipher
 }
